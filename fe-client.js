5668ffd619da3a7217415ee3769fb6d1c27e659c
megamoth <sitoy.arjay@gmail.com>
2017-09-21 17:47:01 +0800

    feat(fe): add fe client script

---
+++ fe-client.js
@@ -0,0 +1,32 @@
+const iframe = document.createElement('iframe');
+const form = document.createElement('form');
+const formInput = document.createElement('input');
+
+iframe.src = 'https://mattermost-staging.identifi.com';
+iframe.id = 'messagelabs-frame';
+iframe.name = 'messagelabs-frame';
+iframe.style.resize = 'both';
+iframe.style.display = 'block';
+iframe.style.marginLeft = 'auto';
+iframe.style.marginRight = 'auto';
+iframe.onload = function onload() {
+  iframe.src = iframe.src;
+  this.onload = null;
+};
+
+document.body.insertBefore(iframe, document.body.firstChild);
+
+const authenticate = (username, password) => {
+  form.target = 'messagelabs-frame';
+  form.action = 'https://mattermost-staging.identifi.com/api/v4/users/login';
+  form.method = 'post';
+  form.enctype = 'text/plain';
+  document.body.insertBefore(form, document.body.firstChild);
+
+  formInput.name = JSON.stringify({ login_id: username, password });
+  formInput.value = '';
+  formInput.type = 'hidden';
+
+  form.insertBefore(formInput, form.firstChild);
+  form.submit();
+};
